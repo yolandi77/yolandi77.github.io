bdba7161ad2bab05d77cb95ef637d66c02f8d594
g <a@b.c>
2025-06-19 12:02:42 +0800

    asdf

--- dist/worker.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log('hello from worker');
-self.addEventListener('message', (event) => {
-    console.log('Received message in Service Worker:', event.data);
-    self.postMessage(event.data());
-});
